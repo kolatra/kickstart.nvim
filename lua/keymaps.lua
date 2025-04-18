b59694a0b3a355971ce8486208e7c5ee40b93cdb
tyler <loukstyler@gmail.com>
2025-04-17 18:30:19 -0600

    add helper function for keybinds

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,53 +1,58 @@
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
-local function setKeybindNormal(key, cmd, desc)
+local function setKeybindN(key, cmd, desc)
   vim.keymap.set('n', key, cmd, { desc = desc })
 end
 
-local function setKeybindVisual(key, cmd, desc)
+local function setKeybindV(key, cmd, desc)
   vim.keymap.set('V', key, cmd, { desc = desc })
+end
+
+local function setKeybindT(key, cmd, desc)
+  vim.keymap.set('t', key, cmd, { desc = desc })
 end
 
 -- Clear highlights on search when pressing <Esc> in normal mode
 --  See `:help hlsearch`
-vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+-- vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
+setKeybindN('<Esc>', '<cmd>nohlsearch<CR>')
 
 -- Diagnostic keymaps
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+-- vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
+setKeybindN('<leader>q', vim.diagnostic.setloclist, 'Open diagnostic [Q]uickfix list')
 
--- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
--- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
--- is not what someone will guess without a bit more experience.
---
--- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
--- or just use <C-\><C-n> to exit terminal mode
-vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+-- vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
+setKeybindT('<Esc><Esc>', '<C-\\><C-n>', 'Exit terminal mode')
 
 -- set keybinds to open [t]erminal, [g]it, [y]azi
-vim.keymap.set('n', '<leader>tg', '<cmd> Term gitui <CR>', { desc = 'Open popup [T]erminal with [G]itUI' })
-vim.keymap.set('n', '<leader>tt', '<cmd> Term <CR>', { desc = 'Open popup [T]erminal' })
-vim.keymap.set('n', '<leader>ty', '<cmd> Term yazi <CR>', { desc = 'Open popup [T]erminal with [Y]azi' })
+-- vim.keymap.set('n', '<leader>tg', '<cmd> Term gitui <CR>', { desc = 'Open popup [T]erminal with [G]itUI' })
+setKeybindN('<leader>tg', '<cmd> Term gitui <CR>', 'Open popup [T]erminal with [G]itUI')
+-- vim.keymap.set('n', '<leader>tt', '<cmd> Term <CR>', { desc = 'Open popup [T]erminal' })
+setKeybindN('<leader>tt', '<cmd> Term <CR>', 'Open popup [T]erminal')
+-- vim.keymap.set('n', '<leader>ty', '<cmd> Term yazi <CR>', { desc = 'Open popup [T]erminal with [Y]azi' })
+setKeybindN('<leader>ty', '<cmd> Term yazi <CR>', 'Open popup [T]erminal with [Y]azi')
 
 -- set a keybind to go back to the file tree
 -- NOTE: i might want to find a new one, or
--- keep using yazi
-vim.keymap.set('n', '<leader>cx', '<cmd> Ex <CR>', { desc = 'Go back to file tree' })
+-- keep using yazi and telescope
 
--- TIP: Disable arrow keys in normal mode
--- vim.keymap.set("n", "<left>", "<cmd>echo "Use h to move!!"<CR>")
--- vim.keymap.set("n", "<right>", "<cmd>echo "Use l to move!!"<CR>")
--- vim.keymap.set("n", "<up>", "<cmd>echo "Use k to move!!"<CR>")
--- vim.keymap.set("n", "<down>", "<cmd>echo "Use j to move!!"<CR>")
+-- vim.keymap.set('n', '<leader>bf', '<cmd> Ex <CR>', { desc = 'Go [B]ack to [F]ile tree' })
+setKeybindN('<leader>bf', '<cmd> Ex <CR>', 'Go [B]ack to [F]ile tree')
 
 -- Keybinds to make split navigation easier.
 --  Use CTRL+<hjkl> to switch between windows
 --
 --  See `:help wincmd` for a list of all window commands
-vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
-vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
-vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
-vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+-- vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+-- vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+-- vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+-- vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+
+setKeybindN('<C-h>', '<C-w><C-h>', 'Move focus to the left window')
+setKeybindN('<C-l>', '<C-w><C-l>', 'Move focus to the right window')
+setKeybindN('<C-j>', '<C-w><C-j>', 'Move focus to the lower window')
+setKeybindN('<C-k>', '<C-w><C-k>', 'Move focus to the upper window')
 
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
